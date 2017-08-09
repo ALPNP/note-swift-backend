4ccecb9495a163acca42c47d07c518b12437f995
filippov <filippov@teleformis.ru>
2017-08-09 19:19:05 +0300

    daysCount for cost rest

--- app/controllers/costs.controller.js
+++ app/controllers/costs.controller.js
@@ -1,13 +1,45 @@
 var Cost = require('./../models/cost');
 var moment = require('./../libs/moment');
-var _ = require('lodash');
 var utilities = require('./../utilities/utilitites');
 
 var costsController = {
+    getCostsChartData: function (req, res) {
+
+        var daysCount = req.headers.dayscount || 7;
+
+        Cost.find({}, function (err, costs) {
+            if (err) {
+                throw err;
+            }
+
+            var result = {
+                content: utilities.createChartDataWithCurrentDayByLastDaysCount(costs, daysCount),
+                daysCount: daysCount
+            };
+
+            return res.json(result);
+        });
+    },
+    getCosts: function (req, res) {
+
+        var daysCount = req.headers.dayscount || 7;
+        var sort = {date: -1};
+
+        Cost.find({}).sort(sort).exec(function (err, costs) {
+            if (err) {
+                throw err;
+            }
+
+            var result = {
+                content: utilities.filterItemsByLastDaysCount(costs, daysCount),
+                daysCount: daysCount
+            };
+
+            return res.json(result);
+        });
+    },
     getCost: function (req, res) {
         Cost.findById(req.params.id || 0, function (err, cost) {
-
-            console.log(cost);
 
             if (cost) {
                 res.json(cost);
@@ -15,8 +47,26 @@ var costsController = {
                 res.json({
                     success: false,
                     message: 'Записей не найдено'
-                })
+                });
             }
+        });
+    },
+    addCost: function (req, res) {
+        var cost = new Cost({
+            date: req.body.date,
+            formatDate: utilities.formatDate(req.body.date),
+            type: req.body.type,
+            amount: req.body.amount,
+            username: req.body.username,
+            description: req.body.description
+        });
+
+        cost.save(function (err) {
+            if (err) {
+                throw err;
+            }
+
+            return res.json({success: true});
         });
     },
     updateCost: function (req, res) {
@@ -38,114 +88,6 @@ var costsController = {
                     res.json(cost);
                 }
             });
-        });
-    },
-    getCostsChartData: function (req, res) {
-        Cost.find({}, function (err, costs) {
-
-            var currentDay = moment().format('L');
-            var currentWeekDays = [];
-            var chartLabels = [];
-            var chartData = [
-                {data: [], label: 'Доходы'},
-                {data: [], label: 'Расходы'}
-            ];
-
-            for (var i = 0; i < 7; i++) {
-                var day = {
-                    date: (function () {
-                        var date = moment().add(-[i], 'd');
-                        return moment(date).format('L');
-                    })(),
-                    costs: []
-                };
-
-                currentWeekDays.push(day);
-            }
-
-            _.forEach(currentWeekDays, function (value) {
-                var searchDay = value;
-                var searchDate = value.date;
-                _.forEach(costs, function (value) {
-                    if (value.formatDate === searchDate) {
-                        searchDay.costs.push(value);
-                    }
-                });
-            });
-
-            _.forEachRight(currentWeekDays, function (day) {
-                chartLabels.push(day.date);
-            });
-
-            _.forEach(chartLabels, function (label) {
-                var filtered = _.filter(costs, function (cost) {
-                    return cost.formatDate === label;
-                });
-
-                var add = 0,
-                    remove = 0;
-
-                if (filtered.length === 0) {
-                    chartData[0].data.push(add);
-                    chartData[1].data.push(remove);
-                } else {
-                    _.forEach(filtered, function (filteredItem) {
-                        if (filteredItem.type === 'add') {
-                            add = filteredItem.amount + add;
-                        } else if (filteredItem.type === 'remove') {
-                            remove = filteredItem.amount + remove;
-                        }
-                    });
-
-                    chartData[0].data.push(add);
-                    chartData[1].data.push(remove);
-                }
-            });
-
-            var result = {
-                content: {
-                    currentDay: currentDay,
-                    chart: {
-                        chartLabels: chartLabels,
-                        chartData: chartData
-                    }
-                }
-            };
-
-            return res.json(result);
-        });
-    },
-    getCosts: function (req, res) {
-        var sort = {date: -1};
-
-        Cost.find({}).sort(sort).exec(function (err, costs) {
-            if (err) {
-                throw err;
-            }
-
-            var result = {
-                content: costs
-            };
-
-            return res.json(result);
-        });
-    },
-    addCost: function (req, res) {
-        var cost = new Cost({
-            date: req.body.date,
-            formatDate: utilities.formatDate(req.body.date),
-            type: req.body.type,
-            amount: req.body.amount,
-            username: req.body.username,
-            description: req.body.description
-        });
-
-        cost.save(function (err) {
-            if (err) {
-                throw err;
-            }
-
-            return res.json({success: true});
         });
     },
     deleteCost: function (req, res) {
